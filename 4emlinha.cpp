02a0894a66b2f27ed935a15de997ea77fa5e6909
Rodrigo Viana <142672337+rpviana@users.noreply.github.com>
2024-02-08 11:35:18 +0000

    Add files via upload

---
+++ 4emlinha.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+void iniciojogo() {
+    // mensagens e in/out de msgs
+    // captacao de decisoes do utilizador
+}
+
+bool terminoujogo() {
+    //verificar se o jogo terminou
+    //alguem ganhou? //tabuleiro cheio??
+}
+
+bool tabuleirocheio() {
+}
+
+bool alguemganhou() {
+}
+
+void mostrartabuleiro() {
+}
+
+int main() {
+    string tabuleiro [8][6]; //// 8 colunas e 6 linhas
+
+    bool tabuleiropreenchido = false;
+    bool jogoterminou = false;
+
+    do {
+        iniciojogo();
+    } while (!jogoterminou);
+
+    return 0;
+
+}
+
+
